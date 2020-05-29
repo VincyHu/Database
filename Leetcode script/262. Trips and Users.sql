e8d9e4f7476dc45607cb0a96cca67815d7592628
Vincy Hu <vincy.hu@ucdavis.edu>
2020-05-29 10:27:21 -0700

    569

--- Leetcode script/262. Trips and Users.sql
+++ Leetcode script/262. Trips and Users.sql
@@ -1,14 +1,11 @@
-/*262. Trips and Users*/
-SELECT t.request_at as 'Day'
-       , ROUND(IFNULL(SUM(CASE WHEN t.Status in ( 'cancelled_by_driver', 'cancelled_by_client') THEN 1 ELSE 0 END)/
-       COUNT(t.id),0),2) as 'Cancellation rate'
+####  Trips and Users ####
+
+SELECT t.Request_at as Day,
+       ROUND(COUNT(CASE WHEN t.status = 'cancelled_by_driver' OR t.status = 'cancelled_by_client' THEN t.status END )/COUNT(*),2) as 'Cancellation Rate'
 FROM trips t 
-LEFT JOIN users client
-    ON t.client_id = client.users_id
-LEFT JOIN users driver
-    ON t.driver_id = driver.users_id
-WHERE client.banned != "Yes" AND driver.banned != "Yes"
-AND t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-GROUP BY t.request_at
-ORDER BY t.request_at
+JOIN users u1 ON t.client_id = u1.users_id 
+JOIN users u2 ON t.driver_id = u2.users_id
+WHERE u1.banned = 'No' AND u2.banned = 'No' AND t.Request_at BETWEEN '2013-10-01' aND '2013-10-03'
+GROUP BY t.Request_at
 ;
+
